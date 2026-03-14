72b6ea827fccf43627b398ac11dce3a44be5f40b
Yaikob/92 <wyaikob@gmail.com>
2026-03-14 17:06:36 +0300

    feat: integrate Clerk for authentication and add auth routes

---
+++ src/app/(auth)/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const AuthScreen = () => {
+
+    
+  return (
+    <SafeAreaView>
+      <Text>AuthScreen</Text>
+    </SafeAreaView>
+  );
+};
+
+export default AuthScreen;
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,14 @@
+import { useAuth } from "@clerk/expo";
+import { Redirect } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function App() {
+  const { isSignedIn } = useAuth();
+
+  if (!isSignedIn) {
+    return <Redirect href="/(auth)" />;
+  }
+
   return (
     <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-xl font-bold text-blue-500">
